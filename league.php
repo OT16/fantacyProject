712736afc64a482945a586faae98ab8aa01c496f
Pauline Wee <paulinekowee@gmail.com>
2024-12-03 16:05:43 -0500

    removed submit claim

--- league.php
+++ league.php
@@ -198,9 +198,6 @@ include ("navbar.html");
                                 <p><strong>Fantasy Points:</strong> <?php echo htmlspecialchars($player['fantasyPoints']); ?></p>
                                 <p><strong>Availability:</strong> <?php echo htmlspecialchars($player['availabilityStatus']); ?></p>
                             </div>
-                            <div class="card-footer">
-                                <a href="claimPlayer.php?playerID=<?php echo $player['playerID']; ?>" class="btn btn-primary">Submit Claim</a>
-                            </div>
                         </div>
                     </div>
                 <?php endforeach; ?>
