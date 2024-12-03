f0a79a69b095e93ef3896971e3249fe773b43e28
Pauline Wee <paulinekowee@gmail.com>
2024-12-02 23:08:29 -0500

    final final final

--- profile.php
+++ profile.php
@@ -30,6 +30,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $updateSqlResult = $conn->query($updateSql);
 }
 
+
+include ('navbar.html');
 ?>
 
 <!DOCTYPE html>
@@ -50,50 +52,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </head>
 <body>
 
-	<!-- Navigation Bar -->
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-		<div class="container-fluid">
-		  <a class="navbar-brand" href="#">
-			<img src="./images/logo.png" style="height: 50px;">
-		  </a>
-		  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		  </button>
-		  <div class="collapse navbar-collapse" id="navbarNav">
-			<ul class="navbar-nav ms-auto">
-			  <li class="nav-item">
-				<a class="nav-link active" href="home.php">Home</a>
-			  </li>
-			  <li class="nav-item">
-				<a class="nav-link" href="profile.php">Profile</a>
-			  </li>
-			</ul>
-		  </div>
-		</div>
-	</nav>
+
 
     <div class="hero">
         <h1>Profile Settings</h1>
     </div>
-
-			<h3 style="text-align: center;">Update Profile Details</h3>
+	<div class="main-container">
+			<h3>Update Profile Details</h3>
 
 			<form class="login-form" action="" method="post">
-				<div class-"profile-section"></div>
+
 					<h5>Change profile fields.</h5>
 					<p>Enter name</p>
 					<input type="text" id="fullName" name="fullName" placeholder="Full Name" value="" required>
 
 					<p>Enter email</p>
 					<input type="email" id="email" name="email"  placeholder="Email" value="" required>
-				</div>
+
 				
-				<div class-"profile-section">
+
 					<input type="submit" value="Save changes" class="btn"/>
-				</div>
+
 
 			</form>
-
+	</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     
 </body>
